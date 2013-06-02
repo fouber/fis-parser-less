2f9af1f81aefb57a9388b22c93956157a0b0f2c9
zhangyunlong <zhangyunlong@baidu.com>
2013-06-02 23:05:45 +0800

    update

--- index.js
+++ index.js
@@ -12,17 +12,7 @@ module.exports = function(content, file, conf){
 	delete conf.rootpath;
     less.render(content, conf, function(e, result){
 		if(e){
-			var msg = 'parser.less: ' + (e.message || e) + ' [' + file.subpath;
-			if(e.hasOwnProperty('line')){
-				msg += ':' + e.line;
-				if(e.hasOwnProperty('col')){
-					msg += ':' + e.col;
-				} else if(e.hasOwnProperty('column')) {
-					msg += ':' + e.column;
-				}
-			}
-			msg += ']';
-			fis.log.error(msg);
+			throw e;
 		} else {
 			content = result;
 		}
