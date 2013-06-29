b980a6ee94d478807c688a8e6d4bc082662a8022
zhangyunlong <zhangyunlong@baidu.com>
2013-06-29 22:51:53 +0800

    update less

--- index.js
+++ index.js
@@ -9,8 +9,6 @@
 var less = require('less');
 
 module.exports = function(content, file, conf){
-	delete conf.rootpath;
-	//conf.paths = [ fis.project.getProjectPath() ];
     less.render(content, conf, function(e, result){
 		if(e){
 			throw e;
