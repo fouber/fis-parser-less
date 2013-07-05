3839b6f5b47f5ba513420606131b8893dc3f671f
zhangyunlong <zhangyunlong@baidu.com>
2013-07-05 23:48:39 +0800

    fix bug

--- index.js
+++ index.js
@@ -7,8 +7,11 @@
 
 
 var less = require('less');
+var root = fis.project.getProjectPath();
 
 module.exports = function(content, file, conf){
+    conf.paths = [ file.dirname, root ];
+    conf.syncImport = true;
     less.render(content, conf, function(e, result){
 		if(e){
 			throw e;
